1b3d6d68690bf32d385168d903597966c51c8e49
Ihor Manzii <igorcollpsp@gmail.com>
2023-12-19 14:24:06 +0100

    Monobank Currency Checker:
     - Test to get currency added;

--- src/test/java/com/ihormanzii/monobankcurrencychecker/MonobankCurrencyCheckerApplicationTests.java
+++ src/test/java/com/ihormanzii/monobankcurrencychecker/MonobankCurrencyCheckerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ihormanzii.monobankcurrencychecker;
 
+import com.ihormanzii.monobankcurrencychecker.feign.dto.CurrencyDTO;
+import com.ihormanzii.monobankcurrencychecker.services.CurrencyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MonobankCurrencyCheckerApplicationTests {
 
+	@Autowired
+	private CurrencyService currencyService;
+
 	@Test
-	void contextLoads() {
+	void getCurrencyTest() {
+		List<CurrencyDTO> currency = currencyService.getCurrency();
+
+		assertThat(currency)
+				.isNotNull()
+				.isNotEmpty();
 	}
 
 }
