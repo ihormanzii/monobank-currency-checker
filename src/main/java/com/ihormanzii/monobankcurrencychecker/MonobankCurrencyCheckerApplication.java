cbe6bc76a19b1c1229970889f892b4632dd438d3
Ihor Manzii <igorcollpsp@gmail.com>
2023-12-19 14:01:15 +0100

    Initial commit;

---
+++ src/main/java/com/ihormanzii/monobankcurrencychecker/MonobankCurrencyCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.ihormanzii.monobankcurrencychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonobankCurrencyCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonobankCurrencyCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihormanzii/monobankcurrencychecker/MonobankCurrencyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihormanzii.monobankcurrencychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonobankCurrencyCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
